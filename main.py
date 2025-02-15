779affe3066379443defca250b6281c45dac677e
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-02-15 18:06:20 +0300

    I added Jinja template and const DATE_FOUNDATION

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import datetime
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+DATE_FOUNDATION = 1920
+
+now = datetime.datetime.now()
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    our_age=now.year - DATE_FOUNDATION,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
