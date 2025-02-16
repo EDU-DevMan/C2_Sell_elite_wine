266324c5dc16d6739cbe0dd2ba92ee552e73e8b4
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-02-16 10:04:56 +0300

    i added func declination_dates

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-DATE_FOUNDATION = 1920
+DATE_FOUNDATION = 2015
 
-now = datetime.datetime.now()
+our_age = datetime.datetime.now().year - DATE_FOUNDATION
+
+
+def declination_dates(foundation):
+    if foundation[-1:] == str(1):
+        return "год"
+    elif foundation[-1:] == str(0) or foundation[-1:] > str(4):
+        return "лет"
+    else:
+        return "года"
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -16,7 +26,8 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    our_age=now.year - DATE_FOUNDATION,
+    our_age=our_age,
+    declin_dates=declination_dates(str(our_age)),
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
